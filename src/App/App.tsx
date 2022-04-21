bde842a361cbf48b8a3c41e31c45bbd90e81d799
Camille Charton <cam2091@gmail.com>
2022-04-21 16:51:13 +0200

    🌱 Initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center">
+      <h3 className="text-white text-2xl">Hello world</h3>
+    </div>
+  );
+}
+
+export default App;
